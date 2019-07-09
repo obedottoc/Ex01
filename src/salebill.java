d3df55619f985b16feeccd0ca22aba6f3c3f788a
user2 <user2@administrator-HP-Pro-3330-MT>
2019-07-09 11:17:36 +0530

    mb

---
+++ src/salebill.java
@@ -0,0 +1,19 @@
+package billings;
+
+import java.until.scanner;
+public class salebill
+{
+	privatre string customer_name;
+	private int customer_no;
+	private double powerrating;
+	private int led_lamps;
+	private String customertype;
+	public salebill()
+	{
+		this.customer_name="bairavan";
+		this.customer_no=10;
+		this.customer powerrating=1000watts;
+		this.led_lamps=2;
+		this.customertype=user
+	}
+}
