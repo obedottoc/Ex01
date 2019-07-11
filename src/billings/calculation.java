d0a6d934a76a72440c1a1bbb4d1168a2ddfbf22a
Karthik Bhaskar <Karthik Bhaskar@DESKTOP-JHFE8LL>
2019-07-11 12:06:56 +0530

    calculation is updated

---
+++ src/billings/calculation.java
@@ -0,0 +1,25 @@
+/******
+ * To calculate the electricity bill amount 
+ * 
+ * Developed by
+ * b.karthik
+ * 
+ * karthikbhaskar2000@gmail.com
+ * 
+ */
+package billings;
+
+public class calculation {
+
+	public static void main(String[] args) {
+		ElectricityBill bill1,bill2;
+		
+		bill1=new ElectricityBill(2000,"kamal","domestic",100,200);
+		bill1.printData();
+		bill2=new ElectricityBill();
+		bill2.getData();
+		bill1.computeBillAmount();
+		bill2.computeBillAmount();
+	}
+
+}
