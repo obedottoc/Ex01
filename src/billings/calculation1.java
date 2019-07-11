d0b7289ef8b930a9b3b36d8f062ffb5d95ab57c0
user2 <user2@administrator-HP-Pro-3330-MT>
2019-07-11 12:10:59 +0530

    vdfd

--- src/billings/calculation1.java
+++ src/billings/calculation1.java
@@ -1,5 +1,26 @@
+
+/******
+ * To calculate the Sale bill amount 
+ * 
+ * Developed by
+ * C.Obed otto
+ * 
+ * obedotto@saveetha.ac.in
+ * 
+ */
 package billings;
 
-public class calculation1 {
+public class Calculation1 {
+
+	public static void main(String[] args) {
+		SaleBill bill1,bill2;
+		
+		bill1=new SaleBill(2001,"Kamal",10,"user",11);
+		bill1.printData();
+		bill2=new SaleBill();
+		bill2.getData();
+		bill1.computeBillAmount();
+		bill2.computeBillAmount();
+	}
 
 }
