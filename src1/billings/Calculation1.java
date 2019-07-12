62908985251a7bdf95e7d1da8a6a06d1b87679f9
user2 <user2@simlab26>
2019-07-12 20:18:20 +0530

    modified

--- src1/billings/Calculation1.java
+++ src1/billings/Calculation1.java
@@ -5,11 +5,11 @@ public class Calculation1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		ElectricityBill bill1,bill2;
+		ElectrictyBill bill1,bill2;
 		
-		bill1=new ElectricityBill (2001,"yogeeswaran");
+		bill1=new ElectrictyBill (2001,"yogeeswaran",123.0,134.6,"Domestic");
 		bill1.printData();
-		bill2=new ElectricityBill ();
+		bill2=new ElectrictyBill ();
 		bill2.getData();
 		bill1.computeBillAmount();
 		bill2.computeBillAmount();
