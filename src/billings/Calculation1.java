ddd48c517e7e30778ec7f17729b0628a147eaef7
UPENDRA NARAYANA <UPENDRA NARAYANA@172.17.86.22>
2019-07-25 20:23:42 +0530

    To calculate electricity bill for different EB connections.

---
+++ src/billings/Calculation1.java
@@ -0,0 +1,25 @@
+/******
+ * To calculate the Sale bill amount 
+ * 
+ * Developed by
+ * M.Upendra
+ * 
+ * upendra1234@gmail.com
+ * 
+ */
+package billings;
+
+public class Calculation1 {
+
+	public static void main(String[] args) {
+		ElectricityBill bill1,bill2;
+
+		bill1=new ElectricityBill(2001,"Kamal",100,200,"domestic");
+		bill1.printData();
+		bill2=new ElectricityBill();
+		bill2.getData();
+		bill1.computeBillAmount();
+		bill2.computeBillAmount();
+	}
+
+}
