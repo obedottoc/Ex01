05d5227a12e90be67d0b60d3a950f0212c79d0dc
sumanth <sumanth@E5-571>
2019-08-04 11:48:29 +0530

    created a electricitybill

---
+++ src/Billings/Calculation1.java
@@ -0,0 +1,15 @@
+package Billings;
+
+public class Calculation1 {
+	public static void main(String[] args) {
+		ElectricityBill bill1,bill2;
+		
+		bill1=new ElectricityBill(2001,"Kamal",0,0,"Domestic");
+		bill1.printData();
+		bill2=new ElectricityBill();
+		bill2.getData();
+		bill1.computeBillAmount();
+		bill2.computeBillAmount();
+	}
+
+}
